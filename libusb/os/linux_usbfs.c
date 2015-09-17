f26feeb05d5e70cf3cfd89b62cb348d3152de0a6
Hyoung Ham <hham@leapmotion.com>
2015-09-17 12:05:35 -0700

    Add #ifdef __ANDROID__ to avoid warnings

--- libusb/os/linux_usbfs.c
+++ libusb/os/linux_usbfs.c
@@ -2795,6 +2795,7 @@ static clockid_t op_get_timerfd_clockid(void)
 }
 #endif
 
+#ifdef __ANDROID__
 static int op_get_device_list(struct libusb_context * ctx,
 	struct discovered_devs **discdevs)
 {
@@ -2855,6 +2856,7 @@ close_out:
 	closedir(devices);
 	return r;
 }
+#endif
 
 const struct usbi_os_backend linux_usbfs_backend = {
 	.name = "Linux usbfs",
