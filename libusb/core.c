38a34dc856f909fabc9e9a422a066860000315f9
Veronica Zheng <vzheng@leapmotion.com>
2016-08-02 13:09:19 -0700

    fix logging

--- libusb/core.c
+++ libusb/core.c
@@ -2420,6 +2420,7 @@ int usbi_gettimeofday(struct timeval *tp, void *tzp)
 static void usbi_log_str(struct libusb_context *ctx,
 	enum libusb_log_level level, const char * str)
 {
+#if defined(USE_SYSTEM_LOGGING_FACILITY)
 #if defined(OS_WINDOWS) || defined(OS_WINCE)
 	/* Windows CE only supports the Unicode version of OutputDebugString. */
 	WCHAR wbuf[USBI_MAX_LOG_LEN];
@@ -2447,6 +2448,9 @@ static void usbi_log_str(struct libusb_context *ctx,
 #warning System logging is not supported on this platform. Logging to stderr will be used instead.
 	fputs(str, stderr);
 #endif
+#else
+	fputs(str, stderr);
+#endif /* USE_SYSTEM_LOGGING_FACILITY */
 	UNUSED(ctx);
 	UNUSED(level);
 }
